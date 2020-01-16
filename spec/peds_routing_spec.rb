7c29780d7e232774619eafc6a0089a9d0ec3d846
Fukuda <29.daikon@gmail.com>
2020-01-16 16:26:33 +0900

    Destroy peds spec

--- spec/peds_routing_spec.rb
+++
@@ -1,9 +0,0 @@
-require "rails_helper"
-
-describe PedsController, type: :routing do
-  describe 'routing' do
-    it 'peds' do
-      expect(get: '/peds').to route_to({"locale"=>"en", "controller"=>"peds", "action"=>"index"})
-    end
-  end
-end
