ff667862a027448e7d1df1a47bf4cbc3f98b64a8
Fukuda <29.daikon@gmail.com>
2020-01-20 17:18:06 +0900

    Get brother component sample

--- app/javascript/packs/controllers/home/page1.vue
+++ app/javascript/packs/controllers/home/page1.vue
@@ -2,6 +2,7 @@
   <div class="page1">
     page1area
     <p>{{ message }}</p>
+    <button @click="callSampleComponent">change</button>
   </div>
 </template>
 
@@ -17,6 +18,12 @@ export default {
   data: function () {
     return {
       message: "Hello Vue Page1!"
+    }
+  },
+  methods: {
+    callSampleComponent: function() {
+      let component = this.$parent.$children.find(child => { return child.$options._componentTag === "sample" })
+      component.message = "changed sample component!"
     }
   }
 }
