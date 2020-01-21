081c52027001344ffed5935c58625af5a7a8d65d
Fukuda <29.daikon@gmail.com>
2020-01-21 19:10:23 +0900

    Add sample test

--- spec/javascript/sample.spec.js
+++ spec/javascript/sample.spec.js
@@ -1,1 +1,14 @@
+import { mount } from '@vue/test-utils'
 import Sample from 'packs/components/sample';
+
+describe('Sample', () => {
+  const wrapper = mount(Sample)
+
+  it('is a Vue instance', () => {
+    expect(wrapper.isVueInstance()).toBeTruthy()
+  })
+
+  it('renders the correct markup', () => {
+    expect(wrapper.html()).toContain('<p>sample component</p>')
+  })
+})
