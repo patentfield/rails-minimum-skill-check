5f1bb1d55323f96bf5d5dcbbaf6d0d868ffdcb99
naoa <visionary.incident@gmail.com>
2021-05-06 10:34:33 +0900

    Simplify and fix pagination bug

--- app/controllers/api/v1/total_page_controller.rb
+++
@@ -1,6 +0,0 @@
-class Api::V1::TotalPageController < ApplicationController
-  def index
-    @results = Uspto::SearchService.new(params[:my_params]).run
-    render json: @results["numFound"]
-  end
-end
